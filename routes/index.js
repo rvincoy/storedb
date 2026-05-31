459506db5fefe8754a6fa30186c3a0375982649c
rvincoy <vin23002@byui.edu>
2026-05-30 22:19:53 -0500

    add ledgers controller and route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/products', require('./products'));
+router.use('/ledgers', require('./ledgers'));
 
 module.exports = router;
